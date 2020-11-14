ff93410db11f2f7b6af1262290ea34b0e05224c1
Rodion Grinberg <42215667+rbeat@users.noreply.github.com>
2020-11-14 21:41:18 +0200

    Update replyButton.js
    Added proper reply format

--- replyButton.js
+++ replyButton.js
@@ -9,7 +9,7 @@
 
 
 var i = 0;
-var replyButtonElement = "<div class=\"zgMAae orLAid magicReplyButton\" data-focus-button-on-close=\"true\"><div role=\"button\" jscontroller=\"VXdfxd\" jsaction=\"focus:AHmuwe; blur:O22p3e;\" jsshadow=\"\" jsname=\"JlEEbd\" class=\"mUbCce n8aqX M9Bg4d\" aria-label=\"Reply\" aria-disabled=\"false\" tabindex=\"0\" data-tooltip=\"Reply\" data-tooltip-vertical-offset=\"-12\" data-tooltip-horizontal-offset=\"0\"><div class=\"\"></div><span jsslot=\"\" class=\"xjKiLb \"><span class=\"\" style=\"top: -10px\"><svg viewBox=\"0 0 24 24\" class=\" f8lxbf UVO9qc ZnfIwf\"><path d=\"M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11z\"></path></svg></span></span></div></div>";
+var replyButtonElement = "<div class=\"zgMAae orLAid magicReplyButton\" data-focus-button-on-close=\"true\"><div role=\"button\" jscontroller=\"VXdfxd\" jsaction=\"focus:AHmuwe; blur:O22p3e;\" jsshadow=\"\" jsname=\"JlEEbd\" class=\"mUbCce n8aqX M9Bg4d\" aria-label=\"Reply\" aria-disabled=\"false\" tabindex=\"0\" data-tooltip=\"Reply\" data-tooltip-vertical-offset=\"-12\" data-tooltip-horizontal-offset=\"0\"><div class=\"\"></div><span jsslot=\"\" class=\"xjKiLb \"><span class=\"\" style=\"top: -10px\"><svg viewBox=\"0 0 24 24\" class=\" f8lxbf UVO9qc ZnfIwf\"><path d=\"M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11z\"></path></svg></span></span></div></div>"
 replyButtonElement = new DOMParser().parseFromString(replyButtonElement, "text/html").body.firstElementChild;
 var actionBubbles = document.getElementsByClassName("eWw5ab");
 for (i = 0; i < actionBubbles.length; i++) {
@@ -20,17 +20,19 @@ for (i = 0; i < actionBubbles.length; i++) {
 var replyButtons = document.getElementsByClassName("magicReplyButton");
 for (i = 0; i < replyButtons.length; i++) {
 	replyButtons[i].addEventListener("click", function (e) {
-
 		if (e.composedPath()[7].innerText == "") {
 			if (e.composedPath()[8].innerText == "") {
-				passMessage("N/A");
+				passMessage("N/A", e.composedPath()[13].innerText.split(/\r?\n/)[0]);
 			} else {
-				passMessage(e.composedPath()[8].innerText);
+				passMessage(e.composedPath()[8].innerText, e.composedPath()[13].innerText.split(/\r?\n/)[0]);
 			}
+		} else {
+			passMessage(e.composedPath()[7].innerText, e.composedPath()[12].innerText.split(/\r?\n/)[0]);
 		}
 	});
 }
 
-function passMessage(message) {
-	document.getElementsByClassName("oAzRtb krjOGe")[0].innerText = message;
+function passMessage(message, author) {
+	document.getElementsByClassName("oAzRtb krjOGe")[0].innerText = "~~~ " + message + " ~~~\n@" + author;
+
 }
